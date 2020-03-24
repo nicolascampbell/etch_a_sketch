359a6db89a755831cc96e7cc552b1ef3fdc8e941
Nicolas <nicodsm7@gmail.com>
2020-03-24 20:17:32 +0100

    added hover on divs

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+
 var divs = new Array(16);
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < divs.length; i++) {
     divs[i]=document.createElement('div');
     document.getElementById("grid_container").appendChild(divs[i]);
 }
+divs.forEach(function(elem) {
+    elem.addEventListener("mouseover", function(event) {
+        event.target.style.backgroundColor="black";
+    });
+});
